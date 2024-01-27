afbb97fb8c299e622034b4294e8e59084c85a05b
Pratham Kandari <112901447+prathamkandari@users.noreply.github.com>
2024-01-28 00:17:28 +0530

    Time: 20 ms (11.20%), Space: 44.1 MB (20.05%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        
+        BigInteger n3 = n1.add(n2);
+        return n3.toString();
+    }
+}
